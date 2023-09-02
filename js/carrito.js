c8dd21b4f93d7497bf035df14fc691a326481631
JSebastianAlises <alisessebastian@gmail.com>
2023-09-01 22:34:46 -0300

    ProyectoFinal

---
+++ js/carrito.js
@@ -0,0 +1,200 @@
+const discosEnCarrito = JSON.parse (localStorage.getItem("carrito"))
+
+const contenedorTarjetasDeCarrito = document.querySelector ("#carrito-de-compras table tbody");
+const contenedorCarritoVacio = document.querySelector ("#contenedor-carrito-vacio");
+const contenedorCarritoDeCompras = document.querySelector ("#carrito-de-compras");
+const contenedorAccionesDeCarrito = document.querySelector ("#acciones-de-carrito");
+const comprar = document.querySelector ("#Comprar");
+const compraRealizada = document.querySelector ("#compra-realizada"); //Mensaje de compra
+const tituloCarritoDeCompras = document.querySelector ("#titulo-carrito-de-compras") 
+
+// CREACIÓN DE TARJETAS EN EL CARRITO DE COMPRAS CON DATOS DEL STORAGE
+function crearTarjetasDeCarrito () {
+
+    contenedorTarjetasDeCarrito.innerHTML = "";
+    //INFORMACIÓN QUE SALE EN CASO DE HABER O NO DATOS Y QUE MOSTRAR
+    if (discosEnCarrito.length > 0) { 
+
+        contenedorCarritoVacio.classList.add("no-figurar");
+        contenedorCarritoDeCompras.classList.remove("no-figurar");
+        contenedorAccionesDeCarrito.classList.remove("no-figurar");
+        compraRealizada.classList.add("no-figurar")
+
+        discosEnCarrito.forEach(disco => {
+        const seleccionDiscos = document.createElement ("tr");
+        seleccionDiscos.classList.add("disco-carrito");
+        seleccionDiscos.innerHTML =`
+            <td><img src= ../img/Discografia/${disco.id}.jpg alt="${disco.titulo}"></td>
+            <td>${disco.titulo}</td>
+            <td><button id="${disco.id}" class="eliminar-cd">-</button>  <span class="cantidad">${disco.cantidad}</span>  <button id="${disco.id}" class="sumar-cd">+</button></td>  
+            <td>${disco.precio}</td>
+            <td>$${parseFloat(disco.precio.replace("$", "")) * parseInt(disco.cantidad)}</td>             
+        `
+        contenedorTarjetasDeCarrito.appendChild (seleccionDiscos)   
+    })}
+    else {
+        contenedorCarritoVacio.classList.remove("no-figurar");
+        contenedorCarritoDeCompras.classList.add("no-figurar");
+        contenedorAccionesDeCarrito.classList.add("no-figurar");
+        compraRealizada.classList.add("no-figurar");
+    }
+    actualizarBotones()
+}
+
+crearTarjetasDeCarrito ()
+
+// BOTON + PARA SUMAR CD
+function sumarDisco (disco) {
+    const cd = disco.currentTarget.id 
+    const restarCd = discosEnCarrito.find (disco => disco.id === cd) 
+
+    if (restarCd.cantidad >= 1) {
+        restarCd.cantidad++; // 
+    }     
+    crearTarjetasDeCarrito ()
+    sincronizarStorage()
+    calcularTotal ()
+    calcularTotalDiscos ()
+}
+
+
+// BOTON - PARA RESTAR CD
+function eliminarDisco (disco) {
+    const cd = disco.currentTarget.id;
+    const indexCd = discosEnCarrito.findIndex (disco => disco.id === cd);
+    let finalCd = 0;
+    discosEnCarrito[indexCd].cantidad --;
+    finalCd = discosEnCarrito[indexCd].cantidad;
+    if (finalCd === 0 ) {
+        discosEnCarrito.splice(indexCd,1)
+    }
+
+    crearTarjetasDeCarrito ()
+    sincronizarStorage()
+    calcularTotal ()
+    calcularTotalDiscos ()
+}
+
+// ACTUALIZACIÓN DE BOTONES PARA PODER USARLOS DE MANERA CONTINÚA
+function actualizarBotones() { 
+    const eliminarCd = document.querySelectorAll(".eliminar-cd");
+    const sumarCd = document.querySelectorAll (".sumar-cd");
+
+    sumarCd.forEach(cd => { 
+        cd.addEventListener("click", sumarDisco);
+    });
+    eliminarCd.forEach(cd => { 
+        cd.addEventListener("click", eliminarDisco);
+    });
+}
+
+// FUNCIONABILIDAD PARA EL BOTON REINICIAR Y ASÍ VACIAR EL CARRITO 
+const btnVaciarCarrito = document.querySelector ("#vaciar-carrito");
+
+btnVaciarCarrito.addEventListener ("click", vaciarCarrito);
+
+function vaciarCarrito () {
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: "Eliminarás todos los discos seleccionados",
+        icon: 'warning',
+        showCancelButton: true,
+        background: "#808080",
+        color:"#fff",
+        confirmButtonColor: '#d45522',
+        cancelButtonColor: '#d45522',
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            background: "#808080",
+            color:"#fff",
+            title: 'Discos eliminados',
+            text:'Si lo deseas, podras volver a la tienda a elegir de nuevo',
+            icon: 'success',
+            confirmButtonText: 'De acuerdo',
+            confirmButtonColor: '#d45522',
+          })
+          discosEnCarrito.length = 0;
+          sincronizarStorage()
+          crearTarjetasDeCarrito ()
+        }
+      })
+}
+
+// CALCULAR EL MONTO TOTAL
+const resultado = document.querySelector ("#resultado")
+
+function calcularTotal () {
+
+    const precioFinal = discosEnCarrito.reduce ((total, album) => {
+        const albumEnDiscosElegidos = discosEnCarrito.find(cd => cd.album === album.album);
+        if (albumEnDiscosElegidos) {
+            const precioTotal = parseFloat(albumEnDiscosElegidos.precio.replace("$", ""));
+            return total + precioTotal * albumEnDiscosElegidos.cantidad;
+        } else {
+            return total;
+        }
+    }, 0);
+    resultado.innerText = precioFinal;
+}
+calcularTotal ()
+
+// INFORMACIÓN DE LA CANTIDAD TOTAL
+const totalDiscos = document.querySelector ("#total-discos")
+
+function calcularTotalDiscos () {
+
+    const discosTotal = discosEnCarrito.reduce ((total, album) => {
+        const albumEnDiscosElegidos = discosEnCarrito.find(cd => cd.album === album.album);
+        if (albumEnDiscosElegidos) {
+            const discosTotal = parseFloat(albumEnDiscosElegidos.cantidad);
+            return total + discosTotal
+        } else {
+            return total;
+        }
+    }, 0);
+    totalDiscos.innerText = discosTotal;
+}
+calcularTotalDiscos ()
+
+// FUNCIONABILIDAD DEL BOTON COMPRAR
+comprar.addEventListener ("click", comprarCarrito)
+
+function comprarCarrito () {
+    Swal.fire({
+        title: '¿Deseas finalizar la compra?',
+        showCancelButton: true,
+        background: "#808080",
+        color:"#fff",
+        confirmButtonColor: '#d45522',
+        cancelButtonColor: '#d45522',
+        confirmButtonText: 'Si, de acuerdo',
+        cancelButtonText: 'No',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            background: "#808080",
+            color:"#fff",
+            title: 'Te agradece tu compra',
+            text:'LOS MISMOS DE SIEMPRE',
+            icon: 'success',
+            confirmButtonColor: '#d45522',
+          })
+          discosEnCarrito.length = 0;
+          sincronizarStorage() 
+          
+          contenedorCarritoVacio.classList.add("no-figurar");
+          contenedorCarritoDeCompras.classList.add("no-figurar");
+          contenedorAccionesDeCarrito.classList.add("no-figurar");
+          tituloCarritoDeCompras.classList.add("no-figurar");
+          compraRealizada.classList.remove("no-figurar");
+        }
+      })
+}
+
+// SINCRONIZACIÓN DE DATOS EN EL STORAGE
+function sincronizarStorage() {
+    localStorage.setItem("carrito", JSON.stringify(discosEnCarrito)) 
+}
---
+++ js/main.js
@@ -0,0 +1,207 @@
+// RELACIONADO A LA UBICACIÓN Y PRODUCCIÓN DE LAS TARJETAS EN EL INICIO CON EL JSON
+
+const contenedorTarjetas = document.querySelector("#discos");
+
+// LLAMO AL JSON CON LOS DATOS DE LA DISCOGRAFIA
+
+function crearTarjetasProductosInicio () {
+    fetch("json/discos.json") 
+    .then((response) => {
+        return response.json()
+        })  
+    .then((datos) => {
+        renderArray(datos) 
+    })
+    .catch((error) => {
+        console.log("error", error);
+    });
+};
+
+// RECORRO TODO EL JSON DE DISCOS Y LO TRAIGO AL INDEX EN FORMATO DE TARJETAS
+function renderArray(discografia) { 
+    discografia.forEach((disco) => { //informo += abajo, para que se pongan uno debajo del otro
+        const tarjetasDeDiscos = document.createElement ("div")
+        tarjetasDeDiscos.classList.add ("disco-tarjeta")
+        tarjetasDeDiscos.innerHTML =`
+                <img src= ./img/Discografia/${disco.id}.jpg alt="${disco.titulo}">
+                <h4>${disco.titulo}</h4>
+                <p>$${disco.precio}</p>     
+                <button class = "comprar-disco" id = ${disco.id}>Agregar al carrito</button>               
+            `
+        contenedorTarjetas.appendChild (tarjetasDeDiscos)
+    });
+}
+
+crearTarjetasProductosInicio()
+
+// RELACIONADO A LA FUNCIONABILIDAD DEL CARRITO EL LOCAL STORAGE
+
+let carritoDeComprasLista = []; // Carrito que quiero llenar con los diferentes discos que vaya a comprar
+
+const discosElegidos = document.querySelector ("#discos-elegidos tbody"); 
+
+// Asignar evento al link de comprar que figura en los discos del HTML
+
+contenedorTarjetas.addEventListener("click", comprarDisco)
+
+// FUNCION QUE REACCIONA AL CLICK EN AGREGAR DISCO
+
+function comprarDisco(evt){
+    evt.preventDefault(); 
+    if(evt.target.classList.contains("comprar-disco")){ 
+        const album = evt.target.parentElement; 
+        albumElegido (album); 
+    }
+    actualizarNumeroCarrito ()
+    calcularTotal ()
+    informacionCarrito ()
+}
+
+// DATOS QUE AGREGO AL CARRITO, COMO LOS IMPRIMO Y FILTRO 
+
+function albumElegido (cd) {
+    const datosDelAlbum = { 
+        album: cd.querySelector ("img").src,
+        titulo: cd.querySelector ("h4").textContent,
+        precio: cd.querySelector ("p").textContent,
+        id: cd.querySelector ("button").getAttribute("id"), 
+        cantidad: 1
+    }
+
+    // FILTRO DE DATOS QUE HAY EN EL CARRITO PARA SUMAR Y EN CASO DE NO HABER, AGREGAR NUEVO DATO
+
+    if(carritoDeComprasLista.some(cd => cd.id === datosDelAlbum.id )){ 
+        const nuevoCarrito = carritoDeComprasLista.map((cd) => { 
+            if (cd.id === datosDelAlbum.id){
+                cd.cantidad++;
+                return cd
+            } else {
+                return cd
+            }     
+        })
+        carritoDeComprasLista = nuevoCarrito.slice(); 
+    } else{
+        carritoDeComprasLista.push(datosDelAlbum)
+    };
+
+    presentacionCarrito () 
+}
+
+// PRESENTACIÓN DE DATOS EN EL CARRITO
+
+function presentacionCarrito () {
+    borrarCarrito() 
+
+    carritoDeComprasLista.forEach(disco => {
+        const seleccionDiscos = document.createElement ("tr")
+        seleccionDiscos.innerHTML =`
+        <td><img src= ./img/Discografia/${disco.id}.jpg alt="${disco.titulo}"></td>
+        <td>${disco.titulo}</td>
+        <td>${disco.precio}</td>
+        <td>${disco.cantidad}</td>
+        `
+        discosElegidos.appendChild (seleccionDiscos)        
+    });
+
+    sincronizarStorage() 
+};
+
+// SINCRONIZACIÓN DE DATOS EN EL STORAGE
+function sincronizarStorage() {
+    localStorage.setItem("carrito", JSON.stringify(carritoDeComprasLista)) 
+}
+
+//BORRO LISTA PREDEFINIDA DEL CARRITO, EVITANDO GENERAR NUEVAS ESTRUCTURAS REPETIDAS DE DATOS
+function borrarCarrito() {
+    while(discosElegidos.firstChild){
+        discosElegidos.removeChild(discosElegidos.firstChild)
+    }
+}
+
+// INFORMO QUE ACCIONES SE DEBEN REALIZAR POSTERIOR A LA CARGA DEL DOM, TENIENDO EN CUENTA SI EL CARRITO TIENE O NO DATOS
+document.addEventListener("DOMContentLoaded", ()=> {
+    if(JSON.parse (localStorage.getItem("carrito"))){ 
+        carritoDeComprasLista = JSON.parse (localStorage.getItem("carrito"));
+    } else {
+        carritoDeComprasLista =[] 
+    }
+    informacionCarrito ()
+    presentacionCarrito() 
+    calcularTotal () 
+})
+
+// FUNCIONABILIDAD DE VACIAR EL CARRITO CON EL BOTON DEL MISMO
+const borrarDiscos = document.querySelector ("#borrar-discos");
+borrarDiscos.addEventListener ("click", borrarDiscosBtn)
+
+function borrarDiscosBtn (evt) {
+    evt.preventDefault();
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: "Eliminarás todos los discos seleccionados",
+        icon: 'warning',
+        showCancelButton: true,
+        background: "#808080",
+        color:"#fff",
+        confirmButtonColor: '#d45522',
+        cancelButtonColor: '#d45522',
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No',
+      }).then((result) => {
+        if (result.isConfirmed) {
+            localStorage.removeItem("carrito"); 
+            carritoDeComprasLista = [];
+            sincronizarStorage();
+            actualizarNumeroCarrito ();
+            informacionCarrito ();
+        }})
+}
+
+// ACTUALIZAR NÚMERO DE CARRITO
+const cantidadCarrito = document.querySelector("#cantidad-carrito")
+
+function actualizarNumeroCarrito () {
+    const memoria = JSON.parse (localStorage.getItem("carrito")); 
+    const total = memoria.reduce((acumulador, discos) => acumulador + discos.cantidad,0) 
+    cantidadCarrito.innerText = total 
+}
+actualizarNumeroCarrito () 
+
+// CALCULAR TOTAL DEL CARRITO
+const resultado = document.querySelector ("#resultado")
+
+function calcularTotal () {
+    const memoria = JSON.parse (localStorage.getItem("carrito"))
+    const precioFinal = memoria.reduce ((total, album) => {
+        const albumEnDiscosElegidos = carritoDeComprasLista.find(cd => cd.album === album.album);
+        if (albumEnDiscosElegidos) {
+            const precioTotal = parseFloat(albumEnDiscosElegidos.precio.replace("$", ""));
+            return total + precioTotal * albumEnDiscosElegidos.cantidad;
+        } else {
+            return total;
+        }
+    }, 0);
+    resultado.innerText = precioFinal;
+}
+
+// INFORMAR SI HAY PRODUCTOS DENTRO DEL CARRITO Y SINO, INDICAR CARRITO VACIO
+const carritoConProductos = document.querySelector ("#carrito-con-productos");
+const carritoVacio = document.querySelector ("#carrito-vacio");
+
+function informacionCarrito () {
+    if (carritoDeComprasLista.length > 0) {
+        carritoConProductos.classList.remove ("no-figurar");
+        carritoVacio.classList.add ("no-figurar");
+    } else {
+        carritoConProductos.classList.add ("no-figurar");
+        carritoVacio.classList.remove ("no-figurar");
+    }
+}
+
+const imagenCarrito = document.querySelector ("#imagen-carrito")
+
+imagenCarrito.addEventListener ("click", quitarFuncion)
+
+function quitarFuncion (evt){
+    evt.preventDefault();
+}
